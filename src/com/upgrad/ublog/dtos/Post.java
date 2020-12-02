cee16dad8d3af1ba1b144eb4ee3d049c8caeaa96
Vikas M <vikas200600@gmail.com>
2020-12-02 16:52:15 +0530

    Exceptions Added

--- src/com/upgrad/ublog/dtos/Post.java
+++ src/com/upgrad/ublog/dtos/Post.java
@@ -1,18 +1,6 @@
 package com.upgrad.ublog.dtos;
 
 import java.time.LocalDateTime;
-/**
- * TODO: 1.6. Declare 6 private instance variables in this class named as postId,
- *  emailId, tag, title, description and timestamp. Out of these 6 variables, postId
- *  will be of type int and timestamp will be of type LocalDateTime
- *  (import java.time.LocalDateTime). Other four variables will be of type String.
- *
- * TODO: 1.7. Provide getters and setters for each of the instance variables. Also,
- *  provide a no-arg constructor and a parameterized constructor to create objects
- *  of Post class.
- *
- * Note: Uncomment the toString() method given below, instead of writing a new one.
- */
 
 public class Post {
     private int postId;
@@ -68,21 +56,4 @@ public class Post {
                 ", timestamp=" + timestamp +
                 '}';
     }
-
-//    public static void main(String[] args) {
-//        Post post = new Post();
-//        post.setPostId(1);
-//        post.setEmailId("dummy@dummy.com");
-//        post.setTag("tag");
-//        post.setTitle("title");
-//        post.setDescription("Description");
-//        post.setTimestamp(LocalDateTime.now());
-//
-//        System.out.println(post);
-//
-//        /**
-//         * Your output should be similar to as shown below.
-//         * Post{postId=1, emailId='dummy@dummy.com', tag='tag', title='title', description='Description', timestamp=2020-05-22T21:01:34.483363800}
-//         */
-//    }
 }
--- src/com/upgrad/ublog/dtos/User.java
+++ src/com/upgrad/ublog/dtos/User.java
@@ -1,17 +1,5 @@
 package com.upgrad.ublog.dtos;
 
-/**
- * TODO: 1.8. Declare 3 private instance variables in this class named as
- *  userId, emailId and password. Out of these 3 variables, userId will be of
- *  type int. Other two variables will be of type String.
- *
- * TODO: 1.9. Provide getters and setters for each of the instance variables. Also,
- *  provide a no-arg constructor and a parameterized constructor to create objects
- *  of User class.
- *
- * Note: Uncomment the toString() method given below, instead of writing a new one.
- */
-
 public class User {
     private int userId;
     private String emailId;
@@ -37,18 +25,4 @@ public class User {
                 ", password='" + password + '\'' +
                 '}';
     }
-
-//    public static void main(String[] args) {
-//        User user = new User();
-//        user.setUserId(1);
-//        user.setEmailId("dummy@dummy.com");
-//        user.setPassword("password");
-//
-//        System.out.println(user);
-//
-//        /**
-//         * Your output should be similar to this.
-//         * User{userId=1, emailId='dummy@dummy.com', password='password'}
-//         */
-//    }
 }
